7dec3aa850b35ae04c27440ff4e5ff28e125988f
jrasini <jespher.ras@protonmail.com>
2020-11-23 09:01:55 -0300

    Numeros e Operacoes Matematicas

---
+++ Cap02/DSA-Python-01-Numeros.py
@@ -0,0 +1,91 @@
+"""  Numeros  """
+# soma
+soma = 4 + 4
+print(soma)
+
+# subtracao
+subtracao = 4 - 3
+print(subtracao)
+
+# multiplicacao
+multiplicacao = 8 * 4
+print(multiplicacao)
+
+# divisao
+divisao = 45 / 9
+print(divisao)
+
+# potencia
+potencia = 4 ** 2
+print(potencia)
+
+# modulo
+modulo = 10 % 3
+print(modulo)
+
+
+"""  funcao type  """
+print(type(5))
+
+print(type(5.0))
+
+a = "Oi"
+print(type(a))
+
+
+"""  Operacoes com numeros float  """
+soma_float = 3.1 + 6.4
+print(soma_float)
+
+soma_float_2 = 4 + 4.0
+print(soma_float_2)
+
+divi_float = 4 / 2
+print(divi_float)
+
+divi = 4 // 2
+print(divi)
+
+c = 4 / 3.0
+print(c)
+
+d = 4 // 3.0
+print(d)
+
+
+""" Conversao """
+print(float(9))
+
+print(int(6.0))
+
+print(int(6.5))
+
+
+""" Hexadecimal e Binario """
+print(hex(394))
+
+print(hex(217))
+
+print(bin(286))
+
+print(bin(390))
+
+""" Funcoes abs, round e pow """
+
+# Retorna o valor absoluto
+print(abs(-8))
+
+# Retorna o valor absoluto
+print(abs(8))
+
+# Retorna o valor com arredondamento
+print(round(3.14151922,2))
+
+# Potencia
+print(pow(4,2))
+
+# Potencia
+print(pow(5,3))
+
+
+
